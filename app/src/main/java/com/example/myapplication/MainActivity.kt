8d2dd0ec68e40e2311dc29d51e90953ea2764b63
zume-zume <88725806+zume-zume@users.noreply.github.com>
2022-08-08 22:39:46 +0900

    【リストビュー】可変リストからアダプタを使ってリストを作成してみる

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // リスト項目をリストとして準備
+        val data = mutableListOf("胡椒", "ターメリック", "サフラン", "ニンニク", "生姜")
+
+        // 配列アダプタを作成し、ListViewに登録
+        val list = findViewById<ListView>(R.id.list)
+        list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
     }
 }
